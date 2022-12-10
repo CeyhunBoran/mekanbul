d9a13787115147cdd7f82379056e2c3d73018bf6
Ceyhun Boran <cey.boran@hotmail.com>
2022-12-10 18:56:28 +0300

    app.js duzeltme

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require("http-errors");
 var express = require("express");
+var session = require("express-session");
 var app = express();
 var path = require("path");
 var cookieParser = require("cookie-parser");
@@ -8,7 +9,6 @@ require("./app_api/models/db");
 var indexRouter = require("./app_server/routes/index");
 var usersRouter = require("./app_server/routes/users");
 var apiRouter = require("./app_api/routes/index");
-var session = require("express-session");
 
 // view engine setup
 app.set("views", path.join(__dirname, "app_server", "views"));
@@ -21,7 +21,7 @@ app.use(session({
 }));
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
